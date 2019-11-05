a754c53317b9f83c928f4e252ede7daf6d6168c2
Jason Loomis <jason@jrloomis.com>
2019-11-05 17:59:02 -0500

    update and delete added

--- cars/carRouter.js
+++ cars/carRouter.js
@@ -14,4 +14,20 @@ router.post("/", (req, res) => {
     .catch(err => res.status(500).json({ error: "500 error" }));
 });
 
+router.put("/:id", (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => res.status(202).json({ message: "Car updated" }))
+    .catch(err => res.status(500).json({ error: "500 error" }));
+});
+
+router.delete("/:id", (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => res.status(200).json({ message: "Car deleted" }))
+    .catch(err => res.status(500).json({ error: "500 error" }));
+});
+
 module.exports = router;
